6e32235b1069a9b7528b74d5dfc92d35f9687598
Esteban Zamora <esteban.zamora.al@gmail.com>
2022-09-28 09:35:42 +0200

    Add matrix plot script for correlation
    Signed-off-by: Esteban Zamora <esteban.zamora.al@gmail.com>

--- project1/scripts/pca.py
+++ project1/scripts/pca.py
@@ -7,14 +7,13 @@ Created on Mon Sep 26 09:35:57 2022
 """
 
 from data import *
-from matplotlib.pyplot import (figure, subplot, plot, xlabel, ylabel, title, 
-yticks, show,legend,imshow,grid,xlim,ylim,arrow,text, cm)
+from matplotlib.pyplot import *
 import scipy.linalg as linalg
 
 # Center the data (subtract mean column values) and divide by the 
 # attribute standard deviation to obtain a standardized dataset
 Xc = X - np.ones((N,1))*X.mean(0)
-Xc = Xc*(1/np.std(Xc,0)) # we could comment this and show the difference in the plots
+Xc = Xc*(1/np.std(Xc,0))
 
 # PCA by computing SVD
 U,S,V = linalg.svd(Xc,full_matrices=False)
@@ -32,12 +31,12 @@ figure()
 plot(range(1,len(rho)+1),rho,'x-')
 plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
 plot([1,len(rho)],[threshold, threshold],'k--')
-title('Variance explained by principal components');
+#title('Variance explained by principal components');
 xlabel('Principal component');
 ylabel('Variance explained');
 legend(['Individual','Cumulative','Threshold'])
 grid()
-title('Variance explained by principal components')
+#title('Variance explained by principal components')
 
 # PCs to plot
 i, j = 0, 1
@@ -56,11 +55,10 @@ grid()
 # Add a unit circle
 plot(np.cos(np.arange(0, 2*np.pi, 0.01)), 
 np.sin(np.arange(0, 2*np.pi, 0.01)));
-title('Attribute coefficients for PCs')
+#title('Attribute coefficients for PCs')
 
 # Plot PCA of the data (PC1 and PC2)
-f = figure()
-title('Rice features projected on PCs')
+figure()
 for c in classDict.values():
     # select indices belonging to class c
     class_mask = (y == c)
@@ -68,3 +66,4 @@ for c in classDict.values():
 legend(classNames)
 xlabel('PC1')
 ylabel('PC2')
+#title('Rice features projected on PCs')
